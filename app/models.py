cb055d0ba8d7f50ead0061cc44e4c670c4aeeb9f
rottengeek <980742271@qq.com>
2018-07-25 00:21:37 +0800

    增加了头像

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import hashlib
 # 使用Werkzeug实现密码散列
 from werkzeug.security import generate_password_hash, check_password_hash
 # 生成具有过期时间的JSON Web签名
@@ -183,6 +184,12 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'https://secure.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
